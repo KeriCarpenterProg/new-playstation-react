601ee22b14c98ebf639aeef8a61d892d36f8f14c
KeriCarpenterProg <56651871+KeriCarpenterProg@users.noreply.github.com>
2023-02-05 18:57:38 -0800

    push

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.css";
+import { Container, Navbar, NavbarBrand } from "reactstrap";
+import NucampLogo from "./app/assets/img/logo.png";
 
 function App() {
-  return <div className="App">Hello Nucamp!</div>;
+  return (
+    <div className="App">
+      <Navbar dark color="primary" sticky="top" expand="md">
+        <Container>
+          <NavbarBrand href="/">
+            <img src={NucampLogo} alt="nucamp logo" />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      I'm ready for workshop!
+    </div>
+  );
 }
 
 export default App;
