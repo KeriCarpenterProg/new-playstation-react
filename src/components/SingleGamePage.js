f1a225ace53c285925403f8289369f4f115bdd61
KeriCarpenterProg <56651871+KeriCarpenterProg@users.noreply.github.com>
2023-02-28 18:17:13 -0800

    single page game

--- src/components/SingleGamePage.js
+++ src/components/SingleGamePage.js
@@ -1,18 +1,50 @@
 import { Col, Row, Container, Card, CardBody, CardHeader } from "reactstrap";
+import { Link } from "react-router-dom";
 import React from "react";
+import "../css/singlegamepage.css";
 
 const SingleGamePage = () => {
   return (
     <Container>
+      <Row>
+        <Col md="4">
+          <img
+            class="w-100"
+            src="https://images.igdb.com/igdb/image/upload/t_original/co5s5v.jpg%22%3E"
+          />
+        </Col>
+        <Col sm="8" className="transparent-box">
+          <h1 className="overflow-wrap">God of War: Ragnarok</h1>
+          <h6>Released: 11/09/2022</h6>
+          <hr />
+          <h6>Genre: Adventure, Hack and slash/Beat 'em up</h6>
+
+          <h6>Platforms: PlayStation 4, PlayStation 5</h6>
+
+          <h6>Editions: See 4 more editions of this game</h6>
+          <br />
+
+          <p>
+            God of War: Ragnarök is the ninth installment in the God of War
+            series and the sequel to 2018's God of War. Continuing with the
+            Norse mythology theme, the game is set in ancient Norway and feature
+            series protagonists Kratos, the former Greek God of War, and his
+            young son Atreus. The game is expected to kick off the events of
+            Ragnarök, where Kratos and Atreus must journey to each of the Nine
+            Realms in search of answers as they prepare for the prophesied
+            battle that will end the world.
+          </p>
+          <hr />
+        </Col>
+      </Row>
       <Row className="row-content">
         <Col sm="6">
-          <h3>God of War: Ragnarok</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Repudiandae enim praesentium fugiat inventore ipsum sapiente in
-            eaque, provident similique ut iure voluptatum expedita amet
-            quisquam.
-          </p>
+          <h3>Migrate the Slick Slider Component here</h3>
+          <h5>
+            <a href="https://new-playstation.web.app/page3.html">
+              Here is what it looks on my bootstrap website. Go to page3.html
+            </a>
+          </h5>
         </Col>
         <Col sm="6">
           <Card>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,7 @@
-import { useState } from "react";
-import { Container, Row, Col } from "reactstrap";
-import CampsiteDetail from "../features/campsites/CampsiteDetail";
-import CampsitesList from "../features/campsites/CampsitesList";
-import { selectCampsiteById } from "../features/campsites/campsitesSlice";
 import PopularGamesSlider from "../components/PopularGamesSlider";
 import RecentlyReviewed from "../components/RecentlyReviewed";
 
 const HomePage = () => {
-  const [campsiteId, setCampsiteId] = useState(0);
-  const selectedCampsite = selectCampsiteById(campsiteId);
   return (
     <>
       <PopularGamesSlider />
