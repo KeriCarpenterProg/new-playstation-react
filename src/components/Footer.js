c004f8815b813a2119f7ea391a42137daad9bbea
Eleanor Kahn <eleanor.c.kahn@gmail.com>
2023-03-02 18:39:31 -0800

    Nav dropdown styling, removed references to DirectoryPage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,9 +15,6 @@ const Footer = () => {
                   <Link to="/">Home</Link>
                 </li>
                 <li>
-                  <Link to="/directory">Directory</Link>
-                </li>
-                <li>
                   <Link to="/about">About</Link>
                 </li>
                 <li>
