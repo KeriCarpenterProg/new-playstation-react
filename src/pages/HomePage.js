db66badca79146e78b94e7740dd112b133476861
Julia Yerman <juliyaerm88@gmail.com>
2023-03-07 18:26:03 -0800

    convert to date

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,6 @@
 import PopularGamesSlider from "../components/PopularGamesSlider";
 import RecentlyReviewed from "../components/RecentlyReviewed";
 import BestGames from "../components/BestGames";
-import BestGameImgs from "../components/BestGameImgs";
 
 const HomePage = () => {
   return (
