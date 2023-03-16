5c6078d1f8cacbd77e29e9f6f90f44ab3763d3db
KeriCarpenterProg <56651871+KeriCarpenterProg@users.noreply.github.com>
2023-03-15 20:38:16 -0700

    extra

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,59 +1,55 @@
 import { Col, Row, Container, Card, CardBody, CardHeader } from "reactstrap";
 
 const AboutPage = () => {
-    return (
-        <Container>
-        <Row className="row-content">
-            <Col sm="6">
-            <h3>Our Mission</h3>
-            <p>
-                We present a curated database of the best campsites in the vast
-                woods and backcountry of the World Wide Web Wilderness. We increase
-                access to adventure for the public while promoting safe and
-                respectful use of resources. The expert wilderness trekkers on our
-                staff personally verify each campsite to make sure that they are up
-                to our standards. We also present a platform for campers to share
-                reviews on campsites they have visited with each other.
-            </p>
-            </Col>
-            <Col sm="6">
-            <Card>
-                <CardHeader className="bg-primary text-white">Hi!</CardHeader>
-                <CardBody>
-                <dl className="row">
-                    <dt className="col-6">Founded</dt>
-                    <dd className="col-6">February 3, 2016</dd>
-                    <dt className="col-6">No. of Campsites in 2019</dt>
-                    <dd className="col-6">563</dd>
-                    <dt className="col-6">No. of Reviews in 2019</dt>
-                    <dd className="col-6">4388</dd>
-                    <dt className="col-6">Employees</dt>
-                    <dd className="col-6">42</dd>
-                </dl>
-                </CardBody>
-            </Card>
-            </Col>
-            <Col>
-            <Card className="bg-light mt-3">
-                <CardBody>
-                <blockquote className="blockquote">
-                    <p>
-                    I will not follow where the path may lead, but I will go where
-                    there is no path, and I will leave a trail.
-                    </p>
-                    <footer className="blockquote-footer">
-                    Muriel Strode,{" "}
-                    <cite title="Source Title">
-                        "Wind-Wafted Wild Flowers" - The Open Court, 1903
-                    </cite>
-                    </footer>
-                </blockquote>
-                </CardBody>
-            </Card>
-            </Col>
-        </Row>
-        </Container>
-    );
+  return (
+    <Container>
+      <Row className="row-content">
+        <Col sm="6">
+          <h3>Contributors</h3>
+          <ul>
+            <li>Julia Yerman</li>
+            <li>Eleanor Hahn</li>
+            <li>Keri Carpenter</li>
+          </ul>
+        </Col>
+        <Col sm="6">
+          <Card>
+            <CardHeader className="bg-primary text-white">Hi!</CardHeader>
+            <CardBody>
+              <dl className="row">
+                <dt className="col-6">Founded</dt>
+                <dd className="col-6">February 3, 2016</dd>
+                <dt className="col-6">No. of Campsites in 2019</dt>
+                <dd className="col-6">563</dd>
+                <dt className="col-6">No. of Reviews in 2019</dt>
+                <dd className="col-6">4388</dd>
+                <dt className="col-6">Employees</dt>
+                <dd className="col-6">42</dd>
+              </dl>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col>
+          <Card className="bg-light mt-3">
+            <CardBody>
+              <blockquote className="blockquote">
+                <p>
+                  I will not follow where the path may lead, but I will go where
+                  there is no path, and I will leave a trail.
+                </p>
+                <footer className="blockquote-footer">
+                  Muriel Strode,{" "}
+                  <cite title="Source Title">
+                    "Wind-Wafted Wild Flowers" - The Open Court, 1903
+                  </cite>
+                </footer>
+              </blockquote>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default AboutPage;
