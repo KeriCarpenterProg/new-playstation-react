824f120a51b46632050cbb9bba2bf94a11b00ab5
KeriCarpenterProg <56651871+KeriCarpenterProg@users.noreply.github.com>
2023-03-27 07:55:40 -0700

    comments are working.

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import { commentsReducer } from "../features/comments/commentsSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    comments: commentsReducer,
   },
 });
