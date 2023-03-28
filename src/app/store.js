43cac0c315f4ce248f167dea48894305e9cfca46
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-03-27 20:48:15 -0700

    gamesSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import { commentsReducer } from "../features/comments/commentsSlice";
+import { gamesReducer } from "../features/games/gamesSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     comments: commentsReducer,
+    games: gamesReducer
   },
 });
