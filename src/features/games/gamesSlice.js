8a3ed6504e796257c033cf6dd71dff5eaa46a97b
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-03-27 20:48:15 -0700

    gamesSlice

--- src/features/games/gamesSlice.js
+++ src/features/games/gamesSlice.js
@@ -1,6 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 import GAMES from "../../app/shared/GAMES";
 
-const intialState = {
+const initialState = {
     games: GAMES,
 };
+
+const gamesSlice = createSlice({
+    name: "games",
+    initialState,
+    reducers: {
+
+    }
+});
+
+export const selectGamesById = (gameId) => {
+    return GAMES.find((game) => game.id === parseInt(gameId));
+};
+
+export const gamesReducer = gamesSlice.reducer;
+
+export const selectAllGames = (state) => state.games;
