8cd4af0d83a6ddd12a544e39cc9525227c7fa004
Julia Yerman <juliyaerm88@gmail.com>
2023-04-09 19:53:47 -0700

    updated About page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,34 +1,68 @@
 import { Col, Row, Container, Card, CardBody, CardHeader } from "reactstrap";
+import "../css/about.css";
 
 const AboutPage = () => {
   return (
     <Container>
       <Row className="row-content">
         <Col sm="6">
+          <h1>About Us</h1>
+          <br />
+          <p class="font-size-14 text-muted">
+            We love games, we grew up with games and now we are doing our dream:
+            working professionally with games.
+            <br />
+            <br /> We do this by...
+            <br />
+            <br />
+            <ol class="font-size-14 text-muted">
+              <li>
+                Gathering all relevant information about games in one place (a
+                concept we call a "one-stop-infospot")
+              </li>
+              <li>
+                Building social and exploratory features on top of this
+                information
+              </li>
+              <li>
+                Gathering a community of both Gamers and people from the game
+                industry and let them communicate with each other
+              </li>
+              <li>
+                Focusing on our users and let them decide on the design and
+                features
+              </li>
+            </ol>
+          </p>
           <h3>Contributors</h3>
-          <ul>
+          <ul class="font-size-14 text-muted">
             <li>Julia Yerman</li>
             <li>Eleanor Kahn</li>
             <li>Keri Carpenter</li>
           </ul>
         </Col>
-        <Col sm="6">
-          <Card>
-            <CardHeader className="bg-primary text-white">Hi!</CardHeader>
-            <CardBody>
-              <dl className="row">
-                <dt className="col-6">Founded</dt>
-                <dd className="col-6">February 3, 2016</dd>
-                <dt className="col-6">No. of Campsites in 2019</dt>
-                <dd className="col-6">563</dd>
-                <dt className="col-6">No. of Reviews in 2019</dt>
-                <dd className="col-6">4388</dd>
-                <dt className="col-6">Employees</dt>
-                <dd className="col-6">42</dd>
-              </dl>
-            </CardBody>
-          </Card>
-        </Col>
+        <Row className="row-content">
+          <Col sm="6">
+            <Card>
+              <CardHeader className="bg-primary text-white">
+                PlayStation Team
+              </CardHeader>
+              <CardBody>
+                <dl className="row">
+                  <dt className="col-6">Founded</dt>
+                  <dd className="col-6">February 3, 2016</dd>
+                  <dt className="col-6">No. of Campsites in 2019</dt>
+                  <dd className="col-6">563</dd>
+                  <dt className="col-6">No. of Reviews in 2019</dt>
+                  <dd className="col-6">4388</dd>
+                  <dt className="col-6">Employees</dt>
+                  <dd className="col-6">42</dd>
+                </dl>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+
         <Col>
           <Card className="bg-light mt-3">
             <CardBody>
