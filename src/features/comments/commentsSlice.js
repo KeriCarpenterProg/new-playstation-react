0a05a68171abbe058caf9741809f3c6d333b0cea
KeriCarpenterProg <56651871+KeriCarpenterProg@users.noreply.github.com>
2023-04-20 20:16:35 -0700

    stuff

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { baseUrl } from "../../app/shared/baseUrl";
-import COMMENTS  from "../../app/shared/COMMENTS";
+import COMMENTS from "../../app/shared/COMMENTS";
 
 const localComments = [...COMMENTS];
 
@@ -12,7 +12,7 @@ export const fetchComments = createAsyncThunk(
       return Promise.reject("Unable to fetch, status: " + response.status);
     }
     const data = await response.json();
-    return data
+    return data;
   }
 );
 
@@ -32,7 +32,7 @@ const commentsSlice = createSlice({
         ...action.payload,
       };
       state.commentsArray.push(newComment);
-    }
+    },
   },
   extraReducers: {
     [fetchComments.pending]: (state) => {
@@ -47,8 +47,10 @@ const commentsSlice = createSlice({
       state.isLoading = false;
       state.errMsg = action.error ? action.error.message : "Fetch failed";
       state.commentsArray = localComments;
-    }
-  }
+      console.log("This is the state.commentsArray. This is keri's work!!!");
+      console.log(state.commentsArray);
+    },
+  },
 });
 
 export const commentsReducer = commentsSlice.reducer;
