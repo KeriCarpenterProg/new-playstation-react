338139920b0e0535520822559e3f906fce7a9fee
Keri Carpenter <keri.carpenter@sony.com>
2025-12-13 17:30:30 -0800

    Add SSL support for Render PostgreSQL connection

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,10 @@
 const { Pool } = require('pg');
 
+const isRenderDatabase = process.env.DATABASE_URL && process.env.DATABASE_URL.includes('render.com');
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: isRenderDatabase ? { rejectUnauthorized: false } : false
 });
 
 pool.on('connect', () => {
