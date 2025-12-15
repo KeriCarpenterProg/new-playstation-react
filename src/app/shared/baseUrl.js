b299853c9709a9b6b90b3c4cee7f4fa6473c6b11
Keri Carpenter <keri.carpenter@sony.com>
2025-12-14 21:49:59 -0800

    hi

--- src/app/shared/baseUrl.js
+++ src/app/shared/baseUrl.js
@@ -1,3 +1,3 @@
 export const baseUrl = process.env.NODE_ENV === 'production'
-  ? process.env.REACT_APP_API_URL || 'https://your-backend.onrender.com'
+  ? process.env.REACT_APP_API_URL || 'https://new-playstation-react.onrender.com'
   : "http://localhost:3001";
